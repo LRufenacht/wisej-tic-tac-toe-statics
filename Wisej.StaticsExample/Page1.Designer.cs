35e1fab70f4904151efcb36675aeaa2df9c576af
Levie Rufenacht <levie.rufenacht@iceteagroup.com>
2025-09-08 20:58:52 -0400

    Add project files.

---
+++ Wisej.StaticsExample/Page1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Wisej.StaticsExample
+{
+    partial class Page1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+		#region Wisej.NET Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new Wisej.Web.Button();
+			SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.CssStyle = "transition: margin 0.4s ease, flex-grow 0.4s ease;";
+			this.button1.Focusable = false;
+			this.button1.Location = new System.Drawing.Point(93, 65);
+			this.button1.Movable = true;
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(100, 37);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.MouseDown += button1_MouseDown;
+			this.button1.MouseMove += button1_MouseMove;
+			this.button1.MouseUp += button1_MouseUp;
+			// 
+			// Page1
+			// 
+			AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+			AutoScaleMode = Web.AutoScaleMode.Font;
+			Controls.Add(this.button1);
+			Name = "Page1";
+			Size = new System.Drawing.Size(2681, 789);
+			Load += Page1_Load;
+			Disposed += Page1_Disposed;
+			ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private Web.Button button1;
+	}
+}
---
+++ Wisej.StaticsExample/Page1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Drawing;
+using Wisej.Web;
+
+namespace Wisej.StaticsExample
+{
+	public partial class Page1 : Page
+	{
+		private static event EventHandler<bool> ButtonMoving;
+
+		private static event EventHandler<Point> ButtonMoved;
+
+		public Page1()
+		{
+			InitializeComponent();
+		}
+
+		private void Page1_Load(object sender, EventArgs e)
+		{
+			ButtonMoved += Page1_ButtonMoved;
+			ButtonMoving += Page1_ButtonMoving;
+		}
+
+		private void Page1_ButtonMoving(object sender, bool e)
+		{
+			Application.Update(this, () =>
+			{
+				// if it's the sender, ignore.
+				if (this._selected)
+					return;
+
+				this.button1.BackColor = e ? Color.Orange : Color.FromName("@button");
+			});
+		}
+
+		private void Page1_ButtonMoved(object sender, Point e)
+		{
+			Application.Update(this, () =>
+			{
+				if (this._selected)
+					return;
+
+				this.button1.Location = new Point(e.X, e.Y);
+			});
+		}
+
+		private void Page1_Disposed(object sender, EventArgs e)
+		{
+			ButtonMoved -= Page1_ButtonMoved;
+		}
+
+		private void button1_MouseDown(object sender, MouseEventArgs e)
+		{
+			this._selected = true;
+			this.button1.BackColor = Color.Red;
+
+			ButtonMoving?.Invoke(this, true);
+		}
+		private bool _selected;
+
+		private void button1_MouseUp(object sender, MouseEventArgs e)
+		{
+			this._selected = false;
+			this.button1.BackColor = Color.FromName("@button");
+
+			ButtonMoving?.Invoke(this, false);
+		}
+
+		private void button1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (this._selected)
+			{
+				// cursor position relative to the button.
+				var cursor = e.Location;
+
+				// cursor position relative to the page.
+				var page = this.button1.PointToScreen(cursor);
+
+				ButtonMoved?.Invoke(this, e.Location);
+			}
+		}
+	}
+}
+  
